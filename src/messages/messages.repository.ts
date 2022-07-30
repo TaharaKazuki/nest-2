a89b14dbd3339953b1248af2c8ad6c495177581f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-07-31 03:25:14 +0900

    update

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,7 +1,26 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class MessageRepository {
-  async findOne(id: string) {}
-  async findAll() {}
-  async create(message: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('message.json', 'utf-8');
+    const message = JSON.parse(contents);
+
+    return message[id];
+  }
+  async findAll() {
+    const contents = await readFile('message.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+  async create(content: string) {
+    const contents = await readFile('message.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('message.json', JSON.stringify(messages));
+  }
 }
